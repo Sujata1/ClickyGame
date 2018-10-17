dc7c3ac3b67dcda3311a5423b971b83bc156171e
Sujata Jhaveri <sujata.jhaveri@gmail.com>
2018-10-17 18:23:05 -0400

    Updates

---
+++ precache-manifest.6d22ddfd2b5b755e9c67dad1ac6d1650.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "f0b7a0c884e6ab7d58ad",
+    "url": "/ClickyGame/static/js/main.f0b7a0c8.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/ClickyGame/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "f0b7a0c884e6ab7d58ad",
+    "url": "/ClickyGame/static/css/main.ddfafd86.chunk.css"
+  },
+  {
+    "revision": "32c4093409983095432ae9d9a7dafd15",
+    "url": "/ClickyGame/index.html"
+  }
+];
